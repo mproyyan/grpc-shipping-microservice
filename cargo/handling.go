e3b20a892bc07ae0ca63f4f5c70d2746c05aed53
mproyyan <mproyyan@gmail.com>
2023-05-09 19:46:57 +0700

    create handling package for handling incoming events

---
+++ cargo/handling.go
@@ -0,0 +1,71 @@
+package cargo
+
+import (
+	"errors"
+
+	"github.com/mproyyan/grpc-shipping-microservice/location"
+	"github.com/mproyyan/grpc-shipping-microservice/voyage"
+)
+
+// HandlingActivity represents how and where a cargo can be handled, and can
+// be used to express predictions about what is expected to happen to a cargo
+// in the future.
+type HandlingActivity struct {
+	Type         HandlingEventType
+	Location     location.UNLocode
+	VoyageNumber voyage.Number
+}
+
+// HandlingEvent is used to register the event when, for instance, a cargo is
+// unloaded from a carrier at a some location at a given time.
+type HandlingEvent struct {
+	// TODO: change TrackingID type to cargo.TrackingID
+	TrackingID string
+	Activity   HandlingActivity
+}
+
+// HandlingEventType describes type of a handling event.
+type HandlingEventType int
+
+// Valid handling event types.
+const (
+	NotHandled HandlingEventType = iota
+	Load
+	Unload
+	Receive
+	Claim
+	Customs
+)
+
+func (t HandlingEventType) String() string {
+	switch t {
+	case NotHandled:
+		return "Not Handled"
+	case Load:
+		return "Load"
+	case Unload:
+		return "Unload"
+	case Receive:
+		return "Receive"
+	case Claim:
+		return "Claim"
+	case Customs:
+		return "Customs"
+	}
+
+	return ""
+}
+
+// HandlingHistory is the handling history of a cargo.
+type HandlingHistory struct {
+	HandlingEvents []HandlingEvent
+}
+
+// MostRecentlyCompletedEvent returns most recently completed handling event.
+func (h HandlingHistory) MostRecentlyCompletedEvent() (HandlingEvent, error) {
+	if len(h.HandlingEvents) == 0 {
+		return HandlingEvent{}, errors.New("delivery history is empty")
+	}
+
+	return h.HandlingEvents[len(h.HandlingEvents)-1], nil
+}

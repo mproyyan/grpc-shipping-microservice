95251907d722907f1a5808bc13c9ef50887edf39
mproyyan <mproyyan@gmail.com>
2023-05-24 08:08:23 +0700

    remove unused column on deliveries table

--- db/schemas/000004_create_delivery_table.up.sql
+++ db/schemas/000004_create_delivery_table.up.sql
@@ -4,11 +4,5 @@ CREATE TABLE IF NOT EXISTS deliveries (
     origin VARCHAR(5) NOT NULL,
     destination VARCHAR(5) NOT NULL,
     arrival_deadline TIMESTAMPTZ,
-    routing_status INT NOT NULL DEFAULT 0,
-    transport_status INT NOT NULL DEFAULT 0,
-    last_event BIGINT REFERENCES events (id),
-    current_voyage VARCHAR(10),
-    eta TIMESTAMPTZ,
-    is_misdirected BOOLEAN NOT NULL DEFAULT false,
-    is_unloaded_at_destination BOOLEAN NOT NULL DEFAULT false
+    last_event BIGINT REFERENCES events (id)
 )
